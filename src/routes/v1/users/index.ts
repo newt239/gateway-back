d40283e743e6528ad53720faefb8ecd7900ceb6c
newt239 <ysti2019.win2020.st@gmail.com>
2021-12-26 14:16:09 +0900

    improve: users/me response

--- src/routes/v1/users/index.ts
+++ src/routes/v1/users/index.ts
@@ -5,7 +5,21 @@ var router = express.Router();
 
 router.get('/me', verifyToken, function (req: express.Request, res: express.Response) {
     const connection = connectDb(res.locals.userid, res.locals.password);
-    return res.json({ userid: res.locals.userid });
+    const sql = `SELECT * FROM gateway.user WHERE userid='${res.locals.userid}'`;
+    connection.query(sql, function (err: any, result: any) {
+        if (err) {
+            return res.json(err);
+        } else {
+            return res.json({
+                userid: result[0].userid,
+                display_name: result[0].display_name,
+                user_type: result[0].user_type,
+                role: result[0].role,
+                available: result[0].available,
+                note: result[0].note,
+            })
+        };
+    });
 });
 
 module.exports = router;
