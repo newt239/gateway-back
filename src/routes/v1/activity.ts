f1e82b14c917972aea9da32fda0e8d24078dcab0
newt239 <ysti2019.win2020.st@gmail.com>
2021-12-28 11:48:21 +0900

    add: create activity endpoints

--- src/routes/v1/activity.ts
+++ src/routes/v1/activity.ts
@@ -3,7 +3,58 @@ import verifyToken from '@/jwt';
 import { connectDb } from '@/db'
 const router = express.Router();
 
-router.get('/enter', verifyToken, function (req: express.Request, res: express.Response) {
+router.post('/enter', verifyToken, function (req: express.Request, res: express.Response) {
+    const connection = connectDb(res.locals.userid, res.locals.password);
+    const guest_id: string = req.body.guest_id;
+    const place_id: string = req.body.place_id;
+    const timestamp = new Date();
+    const activity_id: string = timestamp.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
+    const sql: string = `INSERT * FROM gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES (${activity_id}, ${guest_id}, ${place_id}, ${res.locals.userid}, enter, ${timestamp}, 1)`;
+    connection.query(sql, function (err: any, result: any) {
+        if (err) {
+            return res.json(err);
+        } else {
+            return res.json({
+                status: "success"
+            });
+        };
+    });
+});
+
+router.post('/exit', verifyToken, function (req: express.Request, res: express.Response) {
+    const connection = connectDb(res.locals.userid, res.locals.password);
+    const guest_id: string = req.body.guest_id;
+    const place_id: string = req.body.place_id;
+    const timestamp = new Date();
+    const activity_id: string = timestamp.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
+    const sql: string = `INSERT * FROM gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES (${activity_id}, ${guest_id}, ${place_id}, ${res.locals.userid}, exit, ${timestamp}, 1)`;
+    connection.query(sql, function (err: any, result: any) {
+        if (err) {
+            return res.json(err);
+        } else {
+            return res.json({
+                status: "success"
+            });
+        };
+    });
+});
+
+router.post('/pass', verifyToken, function (req: express.Request, res: express.Response) {
+    const connection = connectDb(res.locals.userid, res.locals.password);
+    const guest_id: string = req.body.guest_id;
+    const place_id: string = req.body.place_id;
+    const timestamp = new Date();
+    const activity_id: string = timestamp.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
+    const sql: string = `INSERT * FROM gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES (${activity_id}, ${guest_id}, ${place_id}, ${res.locals.userid}, pass, ${timestamp}, 1)`;
+    connection.query(sql, function (err: any, result: any) {
+        if (err) {
+            return res.json(err);
+        } else {
+            return res.json({
+                status: "success"
+            });
+        };
+    });
 });
 
 module.exports = router;
