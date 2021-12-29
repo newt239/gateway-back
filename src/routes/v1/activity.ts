eb663eb8fa897e508f38fb7924d756e94a3df3be
newt239 <ysti2019.win2020.st@gmail.com>
2021-12-29 21:10:59 +0900

    change: join activity endpoints

--- src/routes/v1/activity.ts
+++ src/routes/v1/activity.ts
@@ -3,61 +3,26 @@ import verifyToken from '@/jwt';
 import { connectDb } from '@/db'
 const router = express.Router();
 
-router.post('/enter', verifyToken, function (req: express.Request, res: express.Response) {
-    const connection = connectDb(res.locals.userid, res.locals.password);
-    const guest_id: string = req.body.guest_id;
-    const place_id: string = req.body.place_id;
-    const date = new Date();
-    const activity_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
-    const timestamp = date.toISOString().slice(0, 19).replace('T', ' ');
-    const sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${place_id}', '${res.locals.userid}', 'enter', '${timestamp}', 1)`;
-    connection.query(sql, function (err: any, result: any) {
-        if (err) {
-            return res.json(err);
-        } else {
-            return res.json({
-                status: "success"
-            });
-        };
-    });
-});
-
-router.post('/exit', verifyToken, function (req: express.Request, res: express.Response) {
-    const connection = connectDb(res.locals.userid, res.locals.password);
-    const guest_id: string = req.body.guest_id;
-    const place_id: string = req.body.place_id;
-    const date = new Date();
-    const activity_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
-    const timestamp = date.toISOString().slice(0, 19).replace('T', ' ');
-    const sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${place_id}', '${res.locals.userid}', 'exit', '${timestamp}', 1)`;
-    connection.query(sql, function (err: any, result: any) {
-        if (err) {
-            return res.json(err);
-        } else {
-            return res.json({
-                status: "success"
-            });
-        };
-    });
-});
-
-router.post('/pass', verifyToken, function (req: express.Request, res: express.Response) {
-    const connection = connectDb(res.locals.userid, res.locals.password);
-    const guest_id: string = req.body.guest_id;
-    const place_id: string = req.body.place_id;
-    const date = new Date();
-    const activity_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
-    const timestamp = date.toISOString().slice(0, 19).replace('T', ' ');
-    const sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${place_id}', '${res.locals.userid}', 'pass', '${timestamp}', 1)`;
-    connection.query(sql, function (err: any, result: any) {
-        if (err) {
-            return res.json(err);
-        } else {
-            return res.json({
-                status: "success"
-            });
-        };
-    });
+router.post('/:activity_type', verifyToken, function (req: express.Request, res: express.Response) {
+    const activity_type: string = req.params.activity_type;
+    if (activity_type === "enter" || activity_type === "exit" || activity_type === "pass") {
+        const connection = connectDb(res.locals.userid, res.locals.password);
+        const guest_id: string = req.body.guest_id;
+        const place_id: string = req.body.place_id;
+        const date = new Date();
+        const activity_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
+        const timestamp = date.toISOString().slice(0, 19).replace('T', ' ');
+        const sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, place_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${place_id}', '${res.locals.userid}', '${activity_type}', '${timestamp}', 1)`;
+        connection.query(sql, function (err: any, result: any) {
+            if (err) {
+                return res.json(err);
+            } else {
+                return res.json({
+                    status: "success"
+                });
+            };
+        });
+    }
 });
 
 module.exports = router;
