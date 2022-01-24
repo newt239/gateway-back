11fd08ef272aff210d10ebc56c2106d759becbfa
newt239 <ysti2019.win2020.st@gmail.com>
2022-01-24 22:58:58 +0900

    feat: update session table when add activity

--- src/routes/v1/activity.ts
+++ src/routes/v1/activity.ts
@@ -13,11 +13,19 @@ router.post('/:activity_type', verifyToken, function (req: express.Request, res:
         const date = new Date();
         const activity_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
         const timestamp = date.toLocaleString('ja-JP').slice(0, 19).replace('T', ' ');
-        let sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, guest_type, exhibit_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${guest_type}', '${exhibit_id}', '${res.locals.userid}', '${activity_type}', '${timestamp}', 1)`;
-        sql += `UPDATE gateway.guest SET exhibit_id='${activity_type === "enter" ? exhibit_id : ""} WHERE guest_id='${guest_id}''`
+        let sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, guest_type, exhibit_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${guest_type}', '${exhibit_id}', '${res.locals.userid}', '${activity_type}', '${timestamp}', 1);`;
+        sql += `UPDATE gateway.guest SET exhibit_id='${activity_type === "enter" ? exhibit_id : ""}' WHERE guest_id='${guest_id}';`
+        if (activity_type === "enter") {
+            sql += `INSERT INTO gateway.session (session_id, exhibit_id, guest_id, guest_type, enter, available) VALUES ('s${activity_id}', '${exhibit_id}', '${guest_id}', '${guest_type}', '${timestamp}', 1);`
+        } else if (activity_type === "exit") {
+            sql += `UPDATE gateway.session SET exit='${timestamp}' WHERE guest_id='${guest_id}' AND exhibit_id='${exhibit_id}' AND exit=NULL;`;
+        } else if (activity_type === "pass") {
+            sql += `INSERT INTO gateway.session (session_id, exhibit_id, guest_id, guest_type, enter, exit, available) VALUES ('s${activity_id}', '${exhibit_id}', '${guest_id}', '${guest_type}', '${timestamp}', '${timestamp}', 1);`
+        }
         connection.query(sql, function (err: any, result: any) {
             if (err) {
-                return res.status(400).json(err);
+                console.log(err);
+                return res.json(err);
             } else {
                 return res.json({
                     status: "success",
