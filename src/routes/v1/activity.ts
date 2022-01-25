7877426d68a87426212d4d9a30545678a3a71087
newt239 <ysti2019.win2020.st@gmail.com>
2022-01-25 23:41:49 +0900

    improve: exhibit/crowd

--- src/routes/v1/activity.ts
+++ src/routes/v1/activity.ts
@@ -14,11 +14,11 @@ router.post('/:activity_type', verifyToken, function (req: express.Request, res:
         const activity_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
         const timestamp = date.toLocaleString('ja-JP').slice(0, 19).replace('T', ' ');
         let sql: string = `INSERT INTO gateway.activity (activity_id, guest_id, guest_type, exhibit_id, userid, activity_type, timestamp, available) VALUES ('${activity_id}', '${guest_id}', '${guest_type}', '${exhibit_id}', '${res.locals.userid}', '${activity_type}', '${timestamp}', 1);`;
-        sql += `UPDATE gateway.guest SET exhibit_id='${activity_type === "enter" ? exhibit_id : ""}' WHERE guest_id='${guest_id}';`
+        sql += `UPDATE gateway.guest SET exhibit_id=${activity_type === "'enter'" ? exhibit_id : "NULL"} WHERE guest_id='${guest_id}';`
         if (activity_type === "enter") {
             sql += `INSERT INTO gateway.session (session_id, exhibit_id, guest_id, guest_type, enter_at, available) VALUES ('s${activity_id}', '${exhibit_id}', '${guest_id}', '${guest_type}', '${timestamp}', 1);`
         } else if (activity_type === "exit") {
-            sql += `UPDATE gateway.session SET exit='${timestamp}' WHERE guest_id='${guest_id}' AND exhibit_id='${exhibit_id}' AND exit_at IS NULL;`;
+            sql += `UPDATE gateway.session SET exit_at='${timestamp}' WHERE guest_id='${guest_id}' AND exhibit_id='${exhibit_id}' AND exit_at IS NULL;`;
         } else if (activity_type === "pass") {
             sql += `INSERT INTO gateway.session (session_id, exhibit_id, guest_id, guest_type, enter_at, exit_at, available) VALUES ('s${activity_id}', '${exhibit_id}', '${guest_id}', '${guest_type}', '${timestamp}', '${timestamp}', 1);`
         }
--- src/routes/v1/exhibit.ts
+++ src/routes/v1/exhibit.ts
@@ -59,35 +59,15 @@ router.get('/info/:exhibit_id', verifyToken, function (req: express.Request, res
 
 router.get('/crowd/', verifyToken, function (req: express.Request, res: express.Response) {
     const connection = connectDb(res.locals.userid, res.locals.password);
-    const sql: string = `SELECT timestamp(DATE_FORMAT(timestamp, '%Y-%m-%d %H:00:00')) AS timestamp, COUNT(*) AS count FROM gateway.activity GROUP BY DATE_FORMAT(timestamp, '%Y%m%d%H');`;
+    const sql: string = `SELECT timestamp(DATE_FORMAT(enter_at, '%Y-%m-%d %H:00:00')) AS time, COUNT(*) AS count FROM gateway.session GROUP BY DATE_FORMAT(enter_at, '%Y%m%d%H');`;
     connection.query(sql, function (err: any, result: any) {
         console.log(result);
-        const ctime = result[0].timestamp;
-        // TODO: 1件も記録がない時間も配列に含めて返す
-        const data = result.reduce((accumulator: any, element: any) => {
-            const pushList = [];
-            const timestamp = element.timestamp;
-            while (true) {
-                console.log(timestamp, ctime);
-                if (timestamp === ctime) {
-                    pushList.push({ time: timestamp, count: element.count });
-                    break;
-                } else {
-                    const addtime = ctime.setHours(ctime.getHours() + 1);
-                    pushList.push({ time: addtime, count: 0 });
-                    break;
-                };
-            };
-            console.log(accumulator);
-            return accumulator.concat([pushList]);
-        }, []);
-        console.log(data);
         if (err) {
             return res.json(err);
         } else {
             return res.json({
                 status: "success",
-                data: data
+                data: result
             });
         };
     });
