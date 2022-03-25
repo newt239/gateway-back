256426cc2f23855622e913a0e5f2374329c23113
newt239 <ysti2019.win2020.st@gmail.com>
2022-03-25 20:02:35 +0900

    add: insert guest data to session table when regist/revoke guest

--- src/routes/v1/guests.ts
+++ src/routes/v1/guests.ts
@@ -30,6 +30,7 @@ router.get('/info/:guest_id', verifyToken, function (req: express.Request, res: 
 
 router.post('/regist', verifyToken, function (req: express.Request, res: express.Response) {
     const connection = connectDb(res.locals.userid, res.locals.password);
+    // 同じreservationによる複数のguestの登録
     let sql: string = "INSERT INTO gateway.guest (guest_id, guest_type, reservation_id, userid, regist_at, available) VALUES";
     for (const eachRegist of req.body) {
         const guest_id: string = eachRegist.guest_id;
@@ -39,8 +40,19 @@ router.post('/regist', verifyToken, function (req: express.Request, res: express
         sql += `('${guest_id}', '${guest_type}', '${reservation_id}', '${res.locals.userid}', '${timestamp}', 1),`;
     }
     sql = sql.slice(0, -1) + ";";
+    // sessionテーブルへ入場を記録
+    sql += "INSERT INTO gateway.session (session_id, exhibit_id, guest_id, enter_at, enter_operation, available) VALUES"
+    for (const eachRegist of req.body) {
+        const date = new Date();
+        const session_id: string = date.getTime().toString(16) + Math.floor(Math.random() * 10).toString(16);
+        const guest_id: string = eachRegist.guest_id;
+        const timestamp = date.toLocaleString('ja-JP').slice(0, 19).replace('T', ' ');
+        sql += `('${session_id}, 'entrance', ${guest_id}', '${timestamp}', '${res.locals.userid}', 1),`;
+    }
+    sql = sql.slice(0, -1) + ";";
     // 登録したguestの数だけreservation tableのregistedの数を増加させる
     sql += `UPDATE gateway.reservation SET registed = registed + ${req.body.length} WHERE reservation_id='${req.body[0].reservation_id}';`
+    console.log(sql);
     connection.query(sql, function (err: QueryError, result: any) {
         if (err) {
             console.log(err);
@@ -62,12 +74,11 @@ router.post('/regist', verifyToken, function (req: express.Request, res: express
 });
 
 router.post('/revoke', verifyToken, function (req: express.Request, res: express.Response) {
-    // TODO: 同時にactivity tableへの追加も行う...？
     const connection = connectDb(res.locals.userid, res.locals.password);
     const guest_id: string = req.body.guest_id;
-    const guest_type: string = req.body.guest_type;
     const timestamp = new Date().toLocaleString('ja-JP').slice(0, 19).replace('T', ' ');
-    const sql: string = `UPDATE gateway.guest SET revoke_at='${timestamp}' WHERE guest_id='${guest_id}';`;
+    const sql: string = `UPDATE gateway.session SET exit_at='${timestamp}', exit_operation='${res.locals.userid}' WHERE guest_id='${guest_id}' AND exit_at IS NULL;`;
+    console.log(sql);
     connection.query(sql, function (err: any, result: any) {
         if (err) {
             return res.json(err);
