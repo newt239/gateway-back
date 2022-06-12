0c93b8f645903bb19354abd347efd72c077838e4
newt239 <ysti2019.win2020.st@gmail.com>
2022-06-12 21:48:29 +0900

    improve: return capacity and current count at exhibit/info/:exhibit_id

--- routes/exhibit/exhibit.go
+++ routes/exhibit/exhibit.go
@@ -54,6 +54,8 @@ func InfoEachExhibit() echo.HandlerFunc {
 		exhibit_id := c.Param("exhibit_id")
 		var result exhibit
 		db.Where("exhibit_id = ?", exhibit_id).First(&exhibit{}).Scan(&result)
+		var countResult struct{ Current int }
+		db.Raw("select count(*) as current from gateway.session where exhibit_id = '?' and exit_at is null group by guest_id", exhibit_id).Scan(countResult)
 		db.Close()
 		fmt.Println(result)
 		return c.JSON(http.StatusOK, map[string]interface{}{
@@ -61,6 +63,8 @@ func InfoEachExhibit() echo.HandlerFunc {
 			"exhibit_name": result.ExhibitName,
 			"exhibit_type": result.ExhibitType,
 			"room_name":    result.RoomName,
+			"capacity":     result.Capacity,
+			"current":      countResult.Current,
 			"status":       result.Status,
 		})
 	}
